5f0743d6c2a020f35aa3f5c0502a7c2116eff54d
semih <semih__ozsoy@hotmail.com>
2021-04-25 00:25:49 +0300

    adding homework

---
+++ Trendyol-Odev4.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+
+// MARK: ODEV: Girilen sayının asal solup olmadığını bulan bir extension yazınız.
+
+// 2 verilince prime number olma olayını düzeltmen lazım !!!!!
+
+extension Int {
+    
+    func primeNumber() {
+        var isPrime = false
+        
+        for i in 2...self-1{
+            if self % i == 0 {
+                isPrime = true
+                break
+            }
+        }
+         if isPrime == true {
+             print("This is not prime number")
+         }else {
+            print("This is prime number")
+         }
+ 
+    }
+}
+ 
+var number = 32
+number.primeNumber()
+
+
+//MARK: ODEV: İki Parametreli ve Farklı Tipli bir generic örneği yapınız ????
+
+func diffParGeneric<T:Comparable,M:Comparable>(array:[T], secondItem:M)-> T? {
+    
+    for item in array {
+        
+    }
+
+    
+    return nil
+
+}
