78f32132161a3e22254900a2f011e112514f0569
semihozsoy <semih__ozsoy@hotmail.com>
2021-04-30 16:26:35 +0300

    Updating homework 6

--- Trendyol-Odev6.playground/Contents.swift
+++ Trendyol-Odev6.playground/Contents.swift
@@ -3,110 +3,150 @@ import Foundation
 //Game Homework
 
 class Game {
+
+    var gamer : Gamer
+    var ballShooter : BallShooter
+    var bottle : Bottle
     
-    
+    init(gamer:Gamer,ballShooter: BallShooter, bottle: Bottle ) {
+        self.gamer = gamer
+        self.ballShooter = ballShooter
+        self.bottle = bottle
+    }
+ 
+    func getNickName()->String{
+        return gamer.nickName
+    }
+    func bottleLoc(d:Int) {
+       bottle.dRange = d
+    }
+    func ballInfo(teta:Int,v:Int){
+        ballShooter.tetaRange = teta
+        ballShooter.vRange = v
+        
+    }
+    func launching(){
+       let range = ballShooter.range()
+       bottle.shoot(R: range)
+        
+        if bottle.isSteep == false {
+            gamer.point += 1
+        }
+        print(gamer.point)
+    }
+}
+
+class Gamer {
     var nickName: String
     var point: Int
-    
     init(nickName:String,point:Int) {
         self.nickName = nickName
         self.point = point
     }
     
-    
-    func getNickName(nickname:String)->String{
-        return nickname
-    }
-    func bottleLoc(location: Int)->Int {
-        return location
-    }
-    func ballInfo(teta:Int, v:Int){
-    
-    }
-    func launching(){
-        if isSteep == false {
-            point += 1
-        }
-    }
-    
 }
 
-class ballShooter: Game {
+class BallShooter {
     var ballShooter : Int = 0
-    public var teta = 0 ..< 90
-    public var v = 0 ..< 100
+    public var teta = 0...90
+
+    public var v = 0...100
     var g  = 10
     
-    init(ballShooter:Int,g:Int) {
-        self.ballShooter = ballShooter
-        self.g = g
+    init() {
+     
     }
     
     public var tetaRange : Int {
         set {
             if teta ~= newValue {
-                 self.tetaRange = newValue
+                 _tetaRange = newValue
+            }else{
+                print("teta must be in 0 and 90")
             }
         }
         get {
-            return self.tetaRange
+            return _tetaRange
         }
-        
     }
-    
-    
+    private var _tetaRange = 0
+
     public var vRange : Int {
         set {
             if v ~= newValue {
-                self.vRange = newValue
+                _vRange = newValue
+            }else{
+                print("V must be in 0 and 100")
             }
         }
         get {
-            return self.vRange
+            return _vRange
         }
     }
+    private var _vRange = 0
+ 
     
-    
-    func range(r: Int)->Int {
-        let r = sin(2 * teta * Double.pi) / 180
-        let result = v * v * r / g
+    func range()->Double {
+        let r = sin(Double(2 * _tetaRange))
+        let result = Double(_vRange) * Double(_vRange) * Double(r) / Double(g)
        return result
     }
 }
 
-class Bottle:ballShooter {
-    var d = 0 ..< 1500
-    var delta : Int
-    var R : Int
+class Bottle {
+    var d = 0...1500
+    var delta = 0.1 ... 1
     var isSteep = true
     
-    init(delta:Int, R: Int) {
-        self.delta = delta
-        self.R = R
+    init() {
+        
     }
     
     public var dRange : Int {
         set {
             if d ~= newValue {
-                self.dRange = newValue
+                _dRange = newValue
+            }else{
+                print("D must be 0 and 1500")
             }
         }
         get {
-            return self.dRange
+            return _dRange
         }
     }
+    private var _dRange = 0
     
+    public var deltaRange : Double {
+        set {
+            if delta ~= newValue {
+                _deltaRange = newValue
+            }else{
+                print("Delta must be 0.1 and 1")
+            }
+        }
+        get {
+            return _deltaRange
+        }
+    }
+    private var _deltaRange : Double = 0
     
-    func shoot() {
-        if  d - delta < R  && R < d + delta{
+    func shoot(R:Double) {
+        let range = Double(_dRange) - Double(_deltaRange) ... Double(_dRange) + Double(_deltaRange)
+        if  range.contains(R)   {
             isSteep = false
         }else {
             isSteep = true
         }
-        
     }
- 
-    
-    
 }
+
+
+let game = Game(gamer: Gamer(nickName: "semo", point: 0), ballShooter: BallShooter(), bottle: Bottle())
+game.ballInfo(teta: 10, v: 50)
+game.bottleLoc(d:229)
+game.bottle.deltaRange = 0.5
+game.launching()
+
+
+
 
