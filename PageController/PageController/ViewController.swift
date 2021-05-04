887ea73e771956625b8c005fdc665e5c5097b91b
semihozsoy <semih__ozsoy@hotmail.com>
2021-05-04 16:46:19 +0300

    pagecontroller homework initialize

---
+++ PageController/PageController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageController
+//
+//  Created by Semih Özsoy on 4.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
