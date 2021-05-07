5a1c9e6cf5ed17b405cf15d3263865356aeb325a
semihozsoy <semih__ozsoy@hotmail.com>
2021-05-07 17:43:14 +0300

    adding to necessary codes for pagecontroller

--- PageController/PageController/ViewController.swift
+++ PageController/PageController/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var currentPageLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pageControl.numberOfPages = 4
+        pageControl.pageIndicatorTintColor = UIColor.green
+        pageControl.currentPageIndicatorTintColor = UIColor.blue
+        pageControl.addTarget(self, action: #selector(pageChanger(_:)), for: .allTouchEvents)
     }
 
-
+    @IBAction func pageChanger(_ sender: UIPageControl) {
+        
+        currentPageLabel.text = "Current page: \(sender.currentPage+1)"
+        
+        switch sender.currentPage {
+        case 0:
+            self.view.backgroundColor =  UIColor.blue
+        case 1:
+            self.view.backgroundColor = UIColor.red
+        case 2:
+            self.view.backgroundColor = UIColor.cyan
+        case 3:
+            self.view.backgroundColor = UIColor.orange
+        default:
+            self.view.backgroundColor = UIColor.systemGray
+        }
+    }
+    
 }
 
