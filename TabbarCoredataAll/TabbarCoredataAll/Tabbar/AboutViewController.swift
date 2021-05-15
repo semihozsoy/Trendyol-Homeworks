2f32501b9700aae6fd3147c88dd5358524b32dd8
semihozsoy <semih__ozsoy@hotmail.com>
2021-05-15 14:56:03 +0300

    make rounded tabbar item

--- TabbarCoredataAll/TabbarCoredataAll/Tabbar/AboutViewController.swift
+++ TabbarCoredataAll/TabbarCoredataAll/Tabbar/AboutViewController.swift
@@ -11,34 +11,10 @@ class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabbarButtonConfig()
+       
     }
     
-     public func tabbarButtonConfig(){
-        
-        let tabbarItem = UITabBar(frame: CGRect(x: 0, y: 0, width: 128, height: 128))
-        let tabbarButton = UIButton(frame: CGRect(x: 10, y: 10, width: 64, height: 64))
-        
-        var tabbarFrame = tabbarButton.frame
-        tabbarFrame.origin.x = view.bounds.width/2 - tabbarFrame.size.width/2
-        tabbarFrame.origin.y = view.bounds.height - tabbarFrame.size.height
-        
-        
-       // tabbarItem.layer.cornerRadius = tabbarFrame.height/2
-        //view.addSubview(tabbarItem)
-
-//        tabbarButton.layer.cornerRadius = 30
-//        tabbarButton.layer.maskedCorners = [.layerMaxXMinYCorner]
-//        tabbarButton.layer.masksToBounds = true
-//
-        tabbarButton.backgroundColor = UIColor.red
-        tabbarButton.layer.cornerRadius = tabbarFrame.height/2
-        view.addSubview(tabbarButton)
-        tabbarButton.setImage(UIImage(named: "button"), for: .normal)
-        view.layoutIfNeeded()
-
-    }
-   
+    
     
     
     
